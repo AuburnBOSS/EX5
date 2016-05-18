84709cff9602ce7964b594c460b107a65a398b9d
Auburn <stolbennikov@bk.ru>
2016-05-18 22:37:31 +0600

    Немного изменен код, для возможности покрытия его тестами

--- src/main.c
+++ src/main.c
@@ -3,38 +3,7 @@
 #include <locale.h>
 #include <math.h>
 #include "discriminant.h"
-
-const double EPS = 1e-10;
-const short ERROR = -1;
-const short NO_ROOTS = 0;
-const short ONE_ROOT = 1;
-const short TWO_ROOTS = 2;
-
-
-
-int check(double d, double f, double eps)
-{
-	return fabs(d - f) < eps;
-}
-
-short computering_roots(double a, double b, double c, double *x1, double *x2)
-{
-	double D = discriminant(a, b, c);
-	if (D < 0)
-		return NO_ROOTS;
-	if (check(D, 0, EPS))
-	{
-		*x1 =  -b / (2 / a);
-		return ONE_ROOT;
-	}
-	if (D > 0)
-	{
-		*x1 = (-b - sqrt(D)) / (2 * a);
-		*x2 = (-b + sqrt(D)) / (2 * a);
-		return TWO_ROOTS;
-	}
-	return ERROR;
-}
+#include "computering_roots.h"
 
 int main()
 {
